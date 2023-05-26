ac796323b9c7fbc225593539c6ba59a651bf0f01
Morten Holm <33733987+MortenHolmRep@users.noreply.github.com>
2023-05-26 10:40:13 +0200

    Update setup.py
    Better seperation of requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,54 @@
+"""Setup script for DFTBBoost."""
+
 from setuptools import setup, find_packages
 
-# Read the contents of the requirements file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+SETUP_REQUIRES = [
+    "setuptools>=60.0",
+]
+
+# https://pypi.org/classifiers/
+CLASSIFIERS = {
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python :: 3.8",
+    "Environment :: CPU",
+    "Enviornment :: GPU",
+}
+
+INSTALL_REQUIRES = [
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "sciki-learn",
+    "scipy",
+    "pysmiles",
+    "plotly",
+    "tqdm",
+    "torchmetrics",
+    "networkx",
+
+torchmetrics >= 0.11.0
+
+EXTRAS_REQUIRE = {
+    "develop": [
+        "black",
+        "flake8",
+        "docformatter",
+        "isort",
+        "mypy",
+        "autopep8",
+        "pre-commit"
+    ]
+    "GPU": [
+        "torch @ https://download.pytorch.org/whl/cu118'",
+        "torch_geometric >= 2.2",
+        "optuna",
+    ],
+    "CPU": [
+        "torch",
+        "torch_geometric >= 2.2",
+        "optuna",
+    ],
+}
 
 setup(
     name='DFTBBoost',
@@ -10,14 +56,9 @@ setup(
     author='Søren Langkidle',
     author_email='soeren@langkilde.com',
     packages=find_packages(),
-    install_requires=requirements,
+    setup_requires=SETUP_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
     dependency_links=["./source"],
-    extras_require={
-        'GPU': [
-            'torch @ https://download.pytorch.org/whl/cu118'
-        ],
-        'CPU': [
-            'torch'
-        ]
-    }
+    extras_require=EXTRAS_REQUIRE,
+    classifier=CLASSIFIERS,
 )
